f509a4b286c1f81718882d3168ab9385fd83abd6
Rodion Kostin <itrodion@gmail.com>
2022-12-08 23:12:35 +0300

    add bank name normalize

--- utils.py
+++ utils.py
@@ -4,8 +4,16 @@ import re
 def remove_excess_spaces(string):
     return re.sub(' +', ' ', string)
 
+def bank_normalize(facename):
+    FIORE = '[а-яa-zё]+ [а-яa-zё]+ [а-яa-zё]+'
+    m = re.search(f'//({FIORE})//|^({FIORE})$|^({FIORE}) ?/|ИНН \d+ ({FIORE})$|ИНН \d+ ({FIORE}) /', facename, re.IGNORECASE)
+    if m: facename = list(filter(lambda x: x, m.groups()))[0].title()
+    return facename
+
 def normalize_facename(facename):
-    return remove_excess_spaces(facename).replace('\\', '/')
+    facename = bank_normalize(facename)
+    facename = remove_excess_spaces(facename).replace('\\', '/')
+    return facename
 
 def formatDate(date: str):
     d, m, y = date.split('.')
