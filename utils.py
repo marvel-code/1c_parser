1db26577261883ab0cd2d8ad3d2f20e0a02608cb
Rodion Kostin <itrodion@gmail.com>
2022-12-11 20:41:04 +0300

    update normalize_facename

--- utils.py
+++ utils.py
@@ -5,14 +5,15 @@ def remove_excess_spaces(string):
     return re.sub(' +', ' ', string)
 
 def bank_normalize(facename):
-    FIORE = '[а-яa-zё]+ [а-яa-zё]+ [а-яa-zё]+'
-    m = re.search(f'//({FIORE})//|^({FIORE})$|^({FIORE}) ?/|ИНН \d+ ({FIORE})$|ИНН \d+ ({FIORE}) /', facename, re.IGNORECASE)
+    FIORE = '[а-яa-zё\-]+ [а-яa-zё\-]+ [а-яa-zё\-]+'
+    m = re.search(f'//({FIORE})//|^({FIORE})$|^({FIORE}) ?/|ИНН \d+ ({FIORE})$|ИНН \d+ ({FIORE})\s?/', facename, re.IGNORECASE)
     if m: facename = list(filter(lambda x: x, m.groups()))[0].title()
     return facename
 
 def normalize_facename(facename):
     facename = bank_normalize(facename)
-    facename = remove_excess_spaces(facename).replace('\\', '/')
+    facename = remove_excess_spaces(facename)
+    facename = facename.replace('\\', '').replace('/', '')
     return facename
 
 def formatDate(date: str):
